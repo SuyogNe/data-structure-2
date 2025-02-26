295cacb70d6060bc0d31dbb38c04ced90fcc1780
SuyogNe <suyogn78@gmail.com>
2025-02-26 22:03:24 +0545

    set2

---
+++ set2.py
@@ -0,0 +1,6 @@
+a1={"instagram","facebook","twitter","linkedin"}
+a2={"tiktok","instagram","youtube"}
+print("set1: ",a1)
+print("set2: ",a2)
+print("union set: ",a1.union(a2))
+print("intersection set: ",a1.intersection(a2))
