10d4752e1d2691d39d5ec4fb32aa0a8caae0de3d
SuyogNe <suyogn78@gmail.com>
2025-02-26 21:54:39 +0545

    set

---
+++ set.py
@@ -0,0 +1,12 @@
+s={2,2,3,3,4,5,6,6,7,7,8,9,9}
+print(s)
+
+s1={1,2,4,8,16}
+s2={1,2,3,6}
+
+print("set1: ",s1)
+print("set2: ",s2)
+print("\n",s1.difference(s2))
+print(s1.symmetric_difference(s2))
+print(s1.intersection(s2))
+print(s1.union(s2))
